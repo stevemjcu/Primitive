0d3150665f268498822c752f5eb877df966d3198
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-03-01 17:42:22 -0500

    Lay out console app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using System.ComponentModel;
+
+return new CommandApp<RootCommand>().Run(args);
+
+internal sealed class RootCommand : Command<RootCommand.Settings>
+{
+	public class Settings : CommandSettings
+	{
+		[Description("Path to input image")]
+		[CommandArgument(0, "<Input>")]
+		public required string Input { get; set; }
+
+		[Description("Path to output image")]
+		[CommandArgument(1, "[Output]")]
+		public required string Output { get; set; }
+	}
+
+	public override int Execute(CommandContext context, Settings settings)
+	{
+		AnsiConsole.MarkupLine($"Input: [blue]{settings.Input}[/]");
+		AnsiConsole.MarkupLine($"Output: [blue]{settings.Output}[/]");
+		return 0;
+	}
+}
