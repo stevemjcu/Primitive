2ba1e087182694a586613515612eeea435889222
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-03-01 22:37:36 -0500

    Lay out additional arguments

--- Program.cs
+++ Program.cs
@@ -13,14 +13,45 @@ internal sealed class RootCommand : Command<RootCommand.Settings>
 		public required string Input { get; set; }
 
 		[Description("Path to output image")]
-		[CommandArgument(1, "[Output]")]
+		[CommandArgument(1, "<Output>")]
 		public required string Output { get; set; }
+
+		[Description("Type of shape")]
+		[CommandArgument(2, "<Shape>")]
+		public required string Shape { get; set; }
+
+		[Description("Number of shapes")]
+		[CommandArgument(3, "<Iterations>")]
+		public required int Iterations { get; set; }
+
+		[Description("Background color by hex code; averaged if not specified")]
+		[CommandOption("--background")]
+		public string? Background { get; set; }
+
+		[Description("Alpha value of shapes; optimized for if not specified")]
+		[CommandOption("--alpha")]
+		public int? Alpha { get; set; }
+
+		[Description("Dimension to resize input image")]
+		[CommandOption("--input-size")]
+		[DefaultValue(256)]
+		public int InputSize { get; set; }
+
+		[Description("Dimension of output image")]
+		[CommandOption("--output-size")]
+		[DefaultValue(1024)]
+		public int OutputSize { get; set; }
 	}
 
 	public override int Execute(CommandContext context, Settings settings)
 	{
 		AnsiConsole.MarkupLine($"Input: [blue]{settings.Input}[/]");
 		AnsiConsole.MarkupLine($"Output: [blue]{settings.Output}[/]");
+		AnsiConsole.MarkupLine($"Shape: [blue]{settings.Shape}[/]");
+		AnsiConsole.MarkupLine($"Iterations: [blue]{settings.Iterations}[/]");
+
+		// TODO: Configure & run model then process output
+
 		return 0;
 	}
 }
