c6e1e228ce1dced3e1c11e21cff49f5aa5e7114a
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-03-03 21:47:54 -0500

    Implement RMSE helper

--- Helper.cs
+++ Helper.cs
@@ -14,24 +14,41 @@ namespace Primitive
 		public static Color AverageColor(Image<Rgba32> image)
 		{
 			var sum = Vector4.Zero;
+			var pixels = image.Height * image.Width;
 			image.ProcessPixelRows(rows =>
 			{
 				for (var i = 0; i < rows.Height; i++)
 					foreach (var p in rows.GetRowSpan(i))
 						sum += p.ToVector4();
 			});
-			return new Color(sum / (image.Height * image.Width));
+			return new Color(sum / pixels);
 		}
 
 		/// <summary>
 		/// Calculates the root mean squared error between an <see cref="Image{Rgba32}"/> and its target.
+		/// Assumes images are the same dimensions.
 		/// </summary>
 		/// <param name="source">The model <see cref="Image{Rgba32}"/>.</param>
 		/// <param name="target">The ideal <see cref="Image{Rgba32}"/>.</param>
-		/// <returns>The average difference between corresponding color channels.</returns>
+		/// <returns>The average distance between corresponding color channels.</returns>
 		public static int RMSE(Image<Rgba32> source, Image<Rgba32> target)
 		{
-			return default;
+			var sum = Vector4.Zero;
+			var channels = source.Height * source.Width * 4;
+			source.ProcessPixelRows(target, (rows1, rows2) =>
+			{
+				for (var i = 0; i < rows1.Height; i++)
+				{
+					var row1 = rows1.GetRowSpan(i);
+					var row2 = rows2.GetRowSpan(i);
+					for (var j = 0; j < rows1.Width; j++)
+					{
+						var diff = row1[j].ToVector4() - row2[j].ToVector4();
+						sum += diff * diff;
+					}
+				}
+			});
+			return (int)Math.Sqrt((sum.W + sum.X + sum.Y + sum.Z) / channels);
 		}
 	}
 }
