8897adc5067715972274868bc0dfd45c2b9f2d2b
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-03-26 23:10:36 -0400

    Add resource file

--- Program.cs
+++ Program.cs
@@ -8,63 +8,66 @@ using Spectre.Console.Cli;
 using System.ComponentModel;
 using Color = SixLabors.ImageSharp.Color;
 
-var app = new CommandApp<RootCommand>();
+var app = new CommandApp<RootCommand>().WithDescription(Resources.DescriptionApp);
 app.Configure(c => c.SetExceptionHandler((ex, _) => AnsiConsole.WriteException(ex)));
-app.WithDescription("A tool which recreates images using geometric shapes");
 return app.Run(args);
 
 internal sealed class RootCommand : Command<RootCommand.Settings>
 {
 	public class Settings : CommandSettings
 	{
-		[Description("Path to input image")]
+		[ResourceDescription("DescriptionInputPath")]
 		[CommandArgument(0, "<InputPath>")]
 		public required string InputPath { get; set; }
 
-		[Description("Path to output image")]
+		[ResourceDescription("DescriptionOutputPath")]
 		[CommandArgument(1, "<OutputPath>")]
 		public required string OutputPath { get; set; }
 
-		[Description("Type of shape")]
+		[ResourceDescription("DescriptionShape")]
 		[CommandArgument(2, "<Shape>")]
 		public required string Shape { get; set; }
 
-		[Description("Number of shapes")]
+		[ResourceDescription("DescriptionIterations")]
 		[CommandArgument(3, "<Iterations>")]
 		public int Iterations { get; set; }
 
-		[Description("Number of shapes to trial before starting iteration")]
+		[ResourceDescription("DescriptionTrials")]
 		[CommandOption("--trials")]
 		[DefaultValue(200)]
 		public int Trials { get; set; }
 
-		[Description("Number of consecutive failures before ending iteration")]
+		[ResourceDescription("DescriptionFailures")]
 		[CommandOption("--failures")]
 		[DefaultValue(30)]
 		public int Failures { get; set; }
 
-		[Description("Background color hex code")]
+		[ResourceDescription("DescriptionBackground")]
 		[CommandOption("--background")]
 		[DefaultValue("")]
 		public required string Background { get; set; }
 
-		[Description("Dimension to resize input image to")]
-		[CommandOption("--input-size")]
+		[ResourceDescription("DescriptionComputationSize")]
+		[CommandOption("--computation-size")]
 		[DefaultValue(256)]
-		public int InputSize { get; set; }
+		public int ComputationSize { get; set; }
 
-		[Description("Dimension of output image")]
+		[ResourceDescription("DescriptionOutputSize")]
 		[CommandOption("--output-size")]
 		[DefaultValue(1024)]
 		public int OutputSize { get; set; }
 	}
+
+	public sealed class ResourceDescriptionAttribute(string key)
+		: DescriptionAttribute(Resources.ResourceManager.GetString(key)!)
+	{ }
 
 	public override int Execute(CommandContext context, Settings settings)
 	{
 		using var input = Image.Load<Rgba32>(settings.InputPath);
 		input.Mutate(x => x.Resize(new ResizeOptions
 		{
-			Size = new(settings.InputSize, settings.InputSize)
+			Size = new(settings.ComputationSize, settings.ComputationSize)
 		}));
 
 		var model = settings.Background == string.Empty
@@ -74,12 +77,12 @@ internal sealed class RootCommand : Command<RootCommand.Settings>
 		var action = (Action)(settings.Shape switch
 		{
 			"Ellipse" => () => model.Add<Ellipse>(settings.Trials, settings.Failures),
-			_ => throw new Exception("Invalid shape")
+			_ => throw new Exception(Resources.ErrorInvalidShape)
 		});
 
 		AnsiConsole.Progress().Start(ctx =>
 		{
-			var task = ctx.AddTask("[green]Adding shapes[/]", true, settings.Iterations);
+			var task = ctx.AddTask(Resources.DescriptionProgress, true, settings.Iterations);
 			while (!ctx.IsFinished)
 			{
 				action.Invoke();
