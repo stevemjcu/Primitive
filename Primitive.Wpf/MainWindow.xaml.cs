76b6852dfd9bed1cd208ff53eef6273873ab616a
stevemjcu <stephenmjcu@gmail.com>
2026-02-20 15:54:48 -0500

    Add sample code

--- Primitive.Wpf/MainWindow.xaml.cs
+++ Primitive.Wpf/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Primitive.Wpf
 {
@@ -19,6 +10,15 @@ namespace Primitive.Wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
         }
     }
 }
