b319b18191b331e00e83a4a9b67f58828e1299f4
stevemjcu <stephenmjcu@gmail.com>
2026-02-27 12:10:33 -0500

    Set up bindings

--- Primitive.Wpf/MainWindow.xaml.cs
+++ Primitive.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 
 namespace Primitive.Wpf
@@ -8,14 +10,19 @@ namespace Primitive.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<string> Shapes = ["Ellipse"];
+        private readonly List<string> Shapes = ["Ellipse"];
+        private readonly string OutputPath = @"C:\Users\stephen.justice\source\repos\Primitive\Samples\Output.png";
+
+        public ObservableCollection<ComboBoxItem> ShapeItems { get; set; }
+
+        public BitmapImage OutputSource { get; set; }
 
         public MainWindow()
         {
-            InitializeComponent();
+            ShapeItems = new(Shapes.Select(it => new ComboBoxItem() { Content = it }));
+            OutputSource = new(new Uri(OutputPath));
 
-            var uri = @"C:\Users\stephen.justice\source\repos\Primitive\Samples\Output.png";
-            imgOutput.Source = new BitmapImage(new(uri));
+            InitializeComponent();
         }
     }
 }
