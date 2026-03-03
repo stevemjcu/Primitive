c40da972fc77c28c8a22f3ccd1f51d4b5dc00943
stevemjcu <stephenmjcu@gmail.com>
2026-03-03 13:22:28 -0500

    Set up bindings

--- Primitive.Wpf/MainWindow.xaml.cs
+++ Primitive.Wpf/MainWindow.xaml.cs
@@ -10,21 +10,54 @@ namespace Primitive.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly List<string> Shapes = ["Ellipse"];
+        private readonly List<string> ShapeEnum = ["Ellipse"];
         private const string OutputPath = @"C:\Users\stephen.justice\source\repos\Primitive\Samples\Output.png";
+
+        #region Input
 
         public string TargetPath { get; set; } = string.Empty;
 
+        public int WorkingResolutionX { get; set; } = 256;
+
+        public int WorkingResolutionY { get; set; } = 256;
+
+        public int OutputResolutionX { get; set; } = 512;
+
+        public int OutputResolutionY { get; set; } = 512;
+
         public ObservableCollection<ComboBoxItem> ShapeItems { get; set; }
+
+        public int Iterations { get; set; } = 200;
+
+        public int Trials { get; set; } = 100;
+
+        public int Failures { get; set; } = 50;
+
+        #endregion
+
+        #region Output
 
         public BitmapImage OutputSource { get; set; }
 
+        public TimeSpan ElapsedTime { get; set; } = TimeSpan.Zero;
+
+        public int Shapes { get; set; } = 0;
+
+        public float Score { get; set; } = 0;
+
+        #endregion
+
         public MainWindow()
         {
-            ShapeItems = new(Shapes.Select(it => new ComboBoxItem() { Content = it }));
+            ShapeItems = new(ShapeEnum.Select(it => new ComboBoxItem() { Content = it }));
             OutputSource = new(new Uri(OutputPath));
 
             InitializeComponent();
+        }
+
+        private void Step()
+        {
+
         }
     }
 }
